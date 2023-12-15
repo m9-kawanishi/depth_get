61089e2b8c9720469361d27c5324ded8acb8a80d
kawanishi <56061392+m9-kawanishi@users.noreply.github.com>
2023-12-15 15:31:28 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+import cv2
+import pyrealsense2
+from realsense_depth import *
+
+point = (400, 300)
+
+def show_distance(event, x, y, args, params):
+    global point
+    point = (x, y)
+
+# Initialize Camera Intel Realsense
+dc = DepthCamera()
+
+# Create mouse event
+cv2.namedWindow("Color frame")
+cv2.setMouseCallback("Color frame", show_distance)
+
+# 撮影しつづける
+while True:
+    ret, depth_frame, color_frame = dc.get_frame()
+
+    # Show distance for a specific point
+    cv2.circle(color_frame, point, 4, (0, 0, 255))  # マウスの位置にマルを描画
+    distance = depth_frame[point[1], point[0]]  # Depthの値
+
+    # Depthを画面内に描画
+    cv2.putText(color_frame, "{}mm".format(distance), (point[0], point[1] - 20), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2)
+
+    cv2.imshow("depth frame", depth_frame)
+    cv2.imshow("Color frame", color_frame)
+    key = cv2.waitKey(1)
+
+    # Escで終わる
+    if key == 27:
+        break
