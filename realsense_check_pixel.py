027e49e2e5b98a7dcb67a4186289c055d21cb59a
kawanishi <56061392+m9-kawanishi@users.noreply.github.com>
2024-04-04 11:13:56 +0900

    realsenseのピクセル位置の確認ファイルを追加

---
+++ realsense_check_pixel.py
@@ -0,0 +1,71 @@
+import cv2
+import numpy as np
+from realsense_depth import DepthCamera
+from graph import DepthDataVisualizer
+
+class Application:
+    def __init__(self):
+        self.depth_colormap_scale = 0.05
+        self.drawing = False
+        self.ix, self.iy = -1, -1
+        self.ex, self.ey = -1, -1
+        self.dc = DepthCamera()
+        self.mouse_pos = (-1, -1)  # マウス位置を初期化
+
+    def mouse_callback(self, event, x, y, flags, param):
+        if event == cv2.EVENT_MOUSEMOVE:
+            self.mouse_pos = (x, y)  # マウスが動いた場合、その位置を更新
+
+    def draw_rectangle(self, event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            self.drawing = True
+            self.ix, self.iy = x, y
+            self.ex, self.ey = x, y
+
+        elif event == cv2.EVENT_MOUSEMOVE and self.drawing:
+            self.ex, self.ey = x, y
+
+        elif event == cv2.EVENT_LBUTTONUP:
+            self.drawing = False
+            self.ex, self.ey = x, y
+            depth_info = self.get_depth_info(self.ix, self.iy, self.ex, self.ey)
+            a = DepthDataVisualizer()
+            a.plot_data(depth_info)
+            a.show()
+
+    def get_depth_info(self, ix, iy, ex, ey):
+        depth_frame = self.dc.get_frame()[-1]
+        tmp = []
+        for y in range(min(iy, ey), max(iy, ey)):
+            for x in range(min(ix, ex), max(ix, ex)):
+                depth = depth_frame.get_distance(x, y) * 1000  # Convert to mm
+                tmp.append([x, y, depth])
+        return tmp
+
+    def run(self):
+        while True:
+            ret, depth_frame, color_frame, depth_map, frame = self.dc.get_frame()
+            if not ret:
+                break
+
+            if self.ix != -1 and self.iy != -1 and self.ex != -1 and self.ey != -1:
+                cv2.rectangle(color_frame, (self.ix, self.iy), (self.ex, self.ey), (0, 255, 0), 2)
+
+            if self.mouse_pos != (-1, -1):
+                # マウスの位置情報をDepth Frameにテキストとして描画
+                cv2.putText(depth_map, f'Pos: {self.mouse_pos}', (10, 30), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 2)
+
+            cv2.imshow("Depth Frame", depth_map)
+            cv2.imshow("Color Frame", color_frame)
+            cv2.setMouseCallback("Color Frame", self.draw_rectangle)
+            cv2.setMouseCallback("Depth Frame", self.mouse_callback)  # マウスイベントを設定
+
+            if cv2.waitKey(1) & 0xFF == 27:  # ESC key
+                break
+
+        self.dc.release()
+        cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    app = Application()
+    app.run()
